8d19233cfd961b797f152feef83b1b11e567d716
zu21un <jwjoo03@gmail.com>
2022-01-23 00:11:14 +0900

    feat: App.js 라우팅 버튼 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { lazy } from "react";
 import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import styled from 'styled-components';
 
 import './App.css';
 
@@ -9,28 +10,33 @@ import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Editor from './pages/EditorPage';
 
+const StyledDiv = styled.div`
+  margin-top: 100px;
+`;
+
 function App() {
   return (
     <Router>
-      {/* <div> */}
       <Header />
-        {/* <Link to="/login">
-          <button>Login</button>
-        </Link>
-        <Link to="/signup">
-          <button>SignUp</button>
-        </Link>
-        <Link to="/editor">
-          <button>Editor</button>
-        </Link>
-      </div> 
-      <hr /> */}
-      <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/editor" element={<Editor />} />
-      </Routes>
+      <StyledDiv>
+        <hr /> 
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+          <Link to="/signup">
+            <button>SignUp</button>
+          </Link>
+          <Link to="/editor">
+            <button>Editor</button>
+          </Link>
+        <hr /> 
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/editor" element={<Editor />} />
+        </Routes>
+      </StyledDiv>
     </Router>
   );
 }
