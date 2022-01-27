c0332017e186fa77c000787cc35e6dac552bae5e
dlstodyo <dudgks3316@naver.com>
2022-01-27 19:14:16 +0900

    feat: 메인페이지 UI 구현

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,33 +1,26 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
+// import AppBar from '@mui/material/AppBar';
+import { Button, IconButton, Card, CardActions, CardContent, CardMedia, Grid, Box, Typography, Container }from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+// import CameraIcon from '@mui/icons-material/PhotoCamera';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import Stack from '@mui/material/Stack';
+// import Toolbar from '@mui/material/Toolbar';
+// import Link from '@mui/material/Link';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+// function Copyright() {
+//   return (
+//     <Typography variant="body2" color="text.secondary" align="center">
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://mui.com/">
+//         Your Website
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -36,15 +29,6 @@ const theme = createTheme();
 export default function Album() {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <CameraIcon sx={{ mr: 2 }} />
-          <Typography variant="h6" color="inherit" noWrap>
-            Album layout
-          </Typography>
-        </Toolbar>
-      </AppBar>
       <main>
         {/* Hero unit */}
         <Box
@@ -58,18 +42,20 @@ export default function Album() {
             <Typography
               component="h1"
               variant="h2"
-              align="center"
+              align="left"
               color="text.primary"
               gutterBottom
             >
-              Album layout
+              <Button>인기 게시물</Button>
+              <Button>최신 게시물</Button>
+              {/* <Button variant="outlined">이웃 게시물</Button> */}
             </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+            {/* <Typography variant="h5" align="center" color="text.secondary" paragraph>
               Something short and leading about the collection below—its contents,
               the creator, etc. Make it short and sweet, but not too short so folks
               don&apos;t simply skip over it entirely.
-            </Typography>
-            <Stack
+            </Typography> */}
+            {/* <Stack
               sx={{ pt: 4 }}
               direction="row"
               spacing={2}
@@ -77,7 +63,7 @@ export default function Album() {
             >
               <Button variant="contained">Main call to action</Button>
               <Button variant="outlined">Secondary action</Button>
-            </Stack>
+            </Stack> */}
           </Container>
         </Box>
         <Container sx={{ py: 8 }} maxWidth="md">
@@ -107,7 +93,9 @@ export default function Album() {
                     </Typography>
                   </CardContent>
                   <CardActions>
-                    <Button size="small">View</Button>
+                    <IconButton aria-label="like">
+                      <FavoriteIcon />
+                    </IconButton>
                     <Button size="small">Edit</Button>
                   </CardActions>
                 </Card>
@@ -117,7 +105,7 @@ export default function Album() {
         </Container>
       </main>
       {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+      {/* <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
         <Typography variant="h6" align="center" gutterBottom>
           Footer
         </Typography>
@@ -130,7 +118,7 @@ export default function Album() {
           Something here to give the footer a purpose!
         </Typography>
         <Copyright />
-      </Box>
+      </Box> */}
       {/* End footer */}
     </ThemeProvider>
   );
