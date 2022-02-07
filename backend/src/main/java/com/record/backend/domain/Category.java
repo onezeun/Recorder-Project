fd9f879e38dfa98da9bb2f7b0dbbe35f51b816ca
Hyeji Lee <zzambbang@gmail.com>
2022-02-07 16:48:29 +0900

    feat: 도메인 엔티티 양방향 연관관계 메서드 추가

--- backend/src/main/java/com/record/backend/domain/Category.java
+++ backend/src/main/java/com/record/backend/domain/Category.java
@@ -38,4 +38,10 @@ public class Category {
 
 	@OneToMany(mappedBy = "parent") //셀프의 연관관계를 건거라고 생각하면 된다.
 	private List<Category> child = new ArrayList<>();
+
+	//==연관관계 편이 메서드==//
+	public void addChildCategory(Category child) {
+		this.child.add(child);
+		child.setParent(this);
+	}
 }
