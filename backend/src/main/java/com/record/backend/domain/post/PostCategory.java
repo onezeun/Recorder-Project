41ad57a52868b8ce4552a61583c5109829ec4567
Hyeji Lee <zzambbang@gmail.com>
2022-02-09 19:49:27 +0900

    refactor: 불필요한 임포트 리팩토링

--- backend/src/main/java/com/record/backend/domain/post/PostCategory.java
+++ backend/src/main/java/com/record/backend/domain/post/PostCategory.java
@@ -4,13 +4,12 @@ import static javax.persistence.FetchType.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.record.backend.domain.Category;
+import com.record.backend.domain.category.Category;
 
 import lombok.Getter;
 import lombok.Setter;
