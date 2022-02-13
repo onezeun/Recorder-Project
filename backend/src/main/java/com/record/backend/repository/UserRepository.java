9595e4e016e6466004825b0cf6a5e324667b03e8
Hyeji Lee <zzambbang@gmail.com>
2022-02-13 18:46:45 +0900

    feat: post repository, user repository 추가

--- backend/src/main/java/com/record/backend/repository/UserRepository.java
+++ backend/src/main/java/com/record/backend/repository/UserRepository.java
@@ -1,8 +1,14 @@
 package com.record.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.record.backend.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(@Param("email") String email);
+
+	Optional<User> findByDomain(@Param("domain") String domain);
 }
