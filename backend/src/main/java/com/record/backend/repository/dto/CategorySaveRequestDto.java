92a2db83480eb17223b7850561970fd231e13ec5
Hyeji Lee <zzambbang@gmail.com>
2022-02-14 23:54:35 +0900

    feat: category dto 생성

---
+++ backend/src/main/java/com/record/backend/repository/dto/CategorySaveRequestDto.java
@@ -0,0 +1,35 @@
+package com.record.backend.repository.dto;
+
+import java.util.List;
+
+import com.record.backend.domain.category.Category;
+import com.record.backend.domain.post.Post;
+import com.record.backend.domain.user.User;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class CategorySaveRequestDto {
+
+	private User user;
+	private String category_name;
+	private List<Post> postList;
+
+	@Builder
+	public CategorySaveRequestDto(User user, String category_name, List<Post> postList) {
+		this.user = user;
+		this.category_name = category_name;
+		this.postList = postList;
+	}
+
+	public Category toEntity() {
+		return Category.builder()
+			.user(user)
+			.name(category_name)
+			.postList(postList)
+			.build();
+	}
+}
