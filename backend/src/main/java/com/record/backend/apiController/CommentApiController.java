49254807920e57b9cefb9207c13df462195764fb
Hyeji Lee <zzambbang@gmail.com>
2022-02-15 20:10:24 +0900

    feat(controller): comment 컨트롤러 작성

---
+++ backend/src/main/java/com/record/backend/apiController/CommentApiController.java
@@ -0,0 +1,58 @@
+package com.record.backend.apiController;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.record.backend.repository.dto.CommentResponseDto;
+import com.record.backend.repository.dto.CommentSaveRequestDto;
+import com.record.backend.service.CommentService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentApiController {
+
+	private final CommentService commentService;
+
+	@PostMapping("/comments/posts/{post_id}/users/{user_id}")
+	public String saveComment(@PathVariable Long postId, @PathVariable Long userId, CommentSaveRequestDto requestDto) {
+		commentService.saveComment(postId, userId, requestDto);
+		return "redirect:/board/posts/" + postId;
+	}
+
+	@GetMapping("board/comments/{comment_id}")
+	@ResponseBody
+	public CommentResponseDto getComment (@PathVariable Long id) {
+		return commentService.getComment(id);
+	}
+
+	//json 확인용
+	@GetMapping("/comments")
+	@ResponseBody
+	public Page<CommentResponseDto> getAllComments (Pageable pageable) {
+		return commentService.getAllComments(pageable);
+	}
+
+	@PutMapping("/comments/{comment_id}")
+	public String updateComment (@PathVariable Long id, CommentSaveRequestDto requestDto) {
+		Long postId = requestDto.getPost().getId();
+		commentService.updateComment(id, requestDto);
+		return "redirect:/board/posts" + postId;
+	}
+
+	@DeleteMapping("/comments/{comment_id}")
+	public String deleteComment (@PathVariable Long id) {
+		Long postId = commentService.findComment(id).getPost().getId();
+
+		commentService.deleteComment(id);
+		return "redirect:/boards/posts/" + postId;
+	}
+}
