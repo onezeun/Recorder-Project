6da0c21a75310fb0ade85aff96004737073bd829
Hyeji Lee <zzambbang@gmail.com>
2022-02-26 16:20:43 +0900

    feat: 도메인 검증 부분과 생성하는 코드 생성

--- backend/src/main/java/com/record/backend/domain/post/PostTag.java
+++ backend/src/main/java/com/record/backend/domain/post/PostTag.java
@@ -2,6 +2,8 @@ package com.record.backend.domain.post;
 
 import static javax.persistence.FetchType.*;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import com.record.backend.domain.tag.Tag;
+import com.record.backend.exception.IllegalUserException;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -29,4 +32,30 @@ public class PostTag {
 	@ManyToOne(fetch = LAZY)
 	@JoinColumn(name = "tag_id")
 	private Tag tag;
+
+	@Column
+	private boolean deleted;
+
+	private PostTag(Post post, Tag tag) {
+		validateNotNull(post, tag);
+		this.post = post;
+		this.tag = tag;
+	}
+
+	private void validateNotNull(Post post, Tag tag) {
+		if (Objects.isNull(post)) {
+			throw new IllegalUserException("PostTag 생성시 Post는 null이 될 수 없습니다.");
+		}
+		if (Objects.isNull(tag)) {
+			throw new IllegalUserException("PostTag 생성시 Tag는 null이 될 수 없습니다.");
+		}
+	}
+
+	public static PostTag of(Post post, Tag tag) {
+		return new PostTag(post, tag);
+	}
+
+	public void delete() {
+		this.deleted = true;
+	}
 }
