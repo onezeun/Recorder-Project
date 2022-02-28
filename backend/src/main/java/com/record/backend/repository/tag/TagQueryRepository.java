e2fe4378b7729ba41f15423e1bcce6a5199c4ca2
Hyeji Lee <zzambbang@gmail.com>
2022-02-28 14:25:27 +0900

    feat: tag 개발

--- backend/src/main/java/com/record/backend/repository/tag/TagQueryRepository.java
+++ backend/src/main/java/com/record/backend/repository/tag/TagQueryRepository.java
@@ -4,9 +4,12 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
-import org.springframework.data.jpa.repository.query.JpaQueryMethodFactory;
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.record.backend.domain.tag.Tag;
 import com.record.backend.dto.tag.TagDto;
 
 import lombok.RequiredArgsConstructor;
@@ -14,5 +17,27 @@ import lombok.RequiredArgsConstructor;
 @Repository
 @RequiredArgsConstructor
 public class TagQueryRepository {
+
+	@Autowired
+	EntityManager em;
+
+
+	public List<TagDto> findAllTagContatining(String keyword) {
+		if (Objects.isNull(keyword)) {
+			return Collections.emptyList();
+		}
+		em.createQuery("");
+
+		return null;
+	}
+
+	//
+	public List<Tag> findAllByContainsPostName(String postName) {
+		if (Objects.isNull(postName)) {
+			return Collections.emptyList();
+		}
+		return null;
+	}
+
 
 }
