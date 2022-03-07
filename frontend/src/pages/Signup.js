d172dec808f6a057c558e90264644f66fdb56c35
dlstodyo <dudgks3316@naver.com>
2022-03-07 21:56:02 +0900

    feat: 회원가입 기능 구현

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,11 +1,12 @@
 import React, { useState, useCallback } from "react";
-// import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 // import { Router } from 'react-router-dom';
 import { Box, Button, TextField } from "@mui/material";
+import { register } from "../components/Signup/actions/action";
 
+export default function SignUp(props) {
 
-export default function SignUp() {
-  const [password, setPassword] = useState("");
+  const [Password, setPassword] = useState("");
   const onChangePassword = useCallback((e) => {
     setPassword(e.target.value);
   }, []);
@@ -15,9 +16,63 @@ export default function SignUp() {
   const [passwordError, setPasswordError] = useState(false);
   const onChangePasswordCheck = useCallback((e) => {
       setRePassword(e.target.value);
-      setPasswordError(e.target.value !== password);
-    }, [password]);
+      setPasswordError(e.target.value !== Password);
+    }, [Password]);
+    
+  const dispatch = useDispatch();
+  const [Email, setEmail] = useState("");
+  const [Nickname, setNickname] = useState("");
+  const [Domain, setDomain] = useState("");
+  const [Introduce, setIntroduce] = useState("");
 
+  const onEmailHandler = (e) => {
+    setEmail(e.currentTarget.value);
+  }
+
+  // const onPasswordHandler = (e) => {
+  //   setPassword(e.currentTarget.value);
+  // }
+
+  // const onRePasswordHandler = (e) => {
+  //   setRePassword(e.currentTarget.value);
+  // }
+
+  const onNicknameHandler = (e) => {
+    setNickname(e.currentTarget.value);
+  }
+
+  const onDomainHandler = (e) => {
+    setDomain(e.currentTarget.value);
+  }
+
+  const onIntroduceHandler = (e) => {
+    setIntroduce(e.currentTarget.value);
+  }
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log('Email', Email);
+    console.log('Password', Password)
+    console.log('Nickname', Nickname)
+    console.log('Domain', Domain)
+
+    let body = {
+      email: Email,
+      password: Password,
+      nickname: Nickname,
+      domain: Domain,
+      introduce: Introduce
+    }
+
+    dispatch(register(body))
+      .then(response => {
+        if(response.payload.success) {
+          props.history.push('/login')
+        } else {
+          alert('회원가입에 실패하셨습니다.')
+        }
+      })
+  }
 
   return (
     <Box
@@ -44,11 +99,13 @@ export default function SignUp() {
         }}
         noValidate
         autoComplete="off"
+        onSubmit={onSubmitHandler}
       >
         <TextField 
           id="email" 
           label="이메일" 
           variant="outlined"
+          onChange={onEmailHandler}
           />
 
         <TextField
@@ -56,7 +113,7 @@ export default function SignUp() {
           label="비밀번호"
           type="password"
           autoComplete="current-password"
-          value={password}
+          value={Password}
           onChange={onChangePassword}
         />
         <TextField
@@ -67,18 +124,20 @@ export default function SignUp() {
           autoComplete="current-password"
           value={rePassword}
           onChange={onChangePasswordCheck}
-          error={password !== rePassword}
-          helperText={password !== rePassword ? "비밀번호가 일치하지 않습니다" : ""}
+          error={Password !== rePassword}
+          helperText={Password !== rePassword ? "비밀번호가 일치하지 않습니다" : ""}
         />
         <TextField 
-        id="nickname" 
-        label="닉네임" 
-        variant="outlined" 
+          id="nickname" 
+          label="닉네임" 
+          variant="outlined" 
+          onChange={onNicknameHandler}
         />
         <TextField
           id="blogname"
           label="블로그명(영어)"
           variant="outlined"
+          onChange={onDomainHandler}
         />
       </Box>
 
