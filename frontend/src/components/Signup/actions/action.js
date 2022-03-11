f628b3c7baf0aafb851cc281add25340a1eb782f
zu21un <jwjoo03@gmail.com>
2022-03-11 17:03:52 +0900

    fix: signup 로직 수정 완료

--- frontend/src/components/Signup/actions/action.js
+++ frontend/src/components/Signup/actions/action.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { REGISTER_USER } from './types';
 
 export function registerUser(dataTosubmit) {
-  const request = axios.post('http://localhost:8080/api/users/account/signup', dataTosubmit).then((response) => response.data);
-  return { type: REGISTER_USER, payload: request }
+  const request = axios.post('http://localhost:8080/users/account/signup', dataTosubmit)
+    .then((response) => response.data);
+  return { type: REGISTER_USER, payload: request };
 }
