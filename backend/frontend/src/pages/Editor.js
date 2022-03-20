bd9d9d5fef189fcd612f454ab72131155475a895
dlstodyo <dudgks3316@naver.com>
2022-03-20 16:02:57 +0900

    feat: Editor(수정 필요)

--- backend/frontend/src/pages/Editor.js
+++ backend/frontend/src/pages/Editor.js
@@ -251,33 +251,145 @@
 //     );
 // }
 
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import { CKEditor } from '@ckeditor/ckeditor5-react';
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// import ReactHtmlParser from 'react-html-parser';
+
+// export default function Editor() {
+//     const [addData, setVal] = useState('');
+//     const [addedData, showData ] = useState(0);
+//     const handleChange = (e, editor) => {
+//         const data = editor.getData();
+//         setVal(data);
+//     }
+
+//     return (
+//         <div className="Editor">
+//             <h2>
+//                 <u>ckeditor5 with React.js</u>
+//             </h2>
+//             <div style = {{width:'700px', display: 'inline-block', textAlign: 'left'}}>
+//                 <div style={{width:'700px', display: 'inline-block', textAlign: 'right', marginBottom: '5px'}}>
+//                     <button style={{backgroundColor:'black', color:'white'}} onClick={()=>showData(!addedData)}>{addedData ? 'Hide Data' : 'Show Data'}</button>
+//                 </div>
+//                 <CKEditor editor={ClassicEditor} data={addData} onChnage={(e, editor) => { handleChange(e, editor) }} />
+//                 <div>
+//                     {addData}
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// import React, { useState } from 'react';
+// import { CKEditor } from '@ckeditor/ckeditor5-react';
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+
+// export default function Editor() {
+//     const [data, setData] = useState('');
+
+//     const handleChange = (e, editor) => {
+//         setData(ReactHtmlParser(editor.getData()));
+//     }
+//     return (
+//         <div>
+//             <CKEditor
+//                 editor={ClassicEditor}
+//                 onChange={handleChange} />
+//             <div>
+//                 {data}
+//             </div>
+//         </div>
+//     )
+// }
+
+import { useState, useEffect } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import ReactHtmlParser from 'react-html-parser';
+import Axios from 'axios';
 
 export default function Editor() {
-    const [addData, setVal] = useState('');
-    const [addedData, showData ] = useState(0);
-    const handleChange = (e, editor) => {
-        const data = editor.getData();
-        setVal(data);
-    }
+  const [Posts, setPosts] = useState({
+    title: '',
+    content: ''
+  })
 
-    return (
-        <div className="Editor">
-            <h2>
-                <u>ckeditor5 with React.js</u>
-            </h2>
-            <div style = {{width:'700px', display: 'inline-block', textAlign: 'left'}}>
-                <div style={{width:'700px', display: 'inline-block', textAlign: 'right', marginBottom: '5px'}}>
-                    <button style={{backgroundColor:'black', color:'white'}} onClick={()=>showData(!addedData)}>{addedData ? 'Hide Data' : 'Show Data'}</button>
-                </div>
-                <CKEditor editor={ClassicEditor} data={addData} onChnage={handleChange} />
-                <div>
-                    {addedData ? ReactHtmlParser(addData) : ''}
-                </div>
+  const [viewContent, setViewContent] = useState([]);
+
+  useEffect(()=>{
+    Axios.get('http://localhost:8080/board/posts/{post_id}').then((response)=>{
+      setViewContent(response.data);
+    })
+  },[viewContent])
+
+  const submitReview = ()=>{
+    Axios.post('http://localhost:8080/board/posts', {
+      title: Posts.title,
+      content: Posts.content
+    }).then(()=>{
+      alert('등록 완료!');
+    })
+  };
+
+  const getValue = e => {
+    const { name, value } = e.target;
+    setPosts({
+      ...Posts,
+      [name]: value
+    })
+  };
+
+
+  return (
+    <div className="App">
+      <h1>Post Register</h1>
+      <div className='movie-container'>
+        {viewContent.map(element =>
+          <div style={{ border: '1px solid #333' }}>
+            <h2>{element.title}</h2>
+            <div>
+              {ReactHtmlParser(element.content)}
             </div>
-        </div>
-    );
+          </div>
+        )}
+      </div>
+      <div className='form-wrapper'>
+        <input className="title-input"
+          type='text'
+          placeholder='제목'
+          onChange={getValue}
+          name='title'
+        />
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>Hello from CKEditor 5!</p>"
+          onReady={editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+            setPosts({
+              ...Posts,
+              content: data
+            })
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
+      </div>
+      <button
+        className="submit-button"
+        onClick={submitReview}
+        >입력</button>
+    </div>
+  );
 }
