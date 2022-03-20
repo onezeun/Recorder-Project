6ecddba984ac2bc89963ec9a0c2440281b76e453
Hyeji Lee <zzambbang@gmail.com>
2022-03-20 20:35:17 +0900

    error: 회원가입 안되는 오류 수정

--- backend/src/main/java/com/record/backend/domain/user/User.java
+++ backend/src/main/java/com/record/backend/domain/user/User.java
@@ -11,6 +11,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -31,10 +32,10 @@ import lombok.Setter;
 
 @Getter @Setter
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User extends BaseEntity {
 
-	@Id @GeneratedValue
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id")
 	private Long id;
 
