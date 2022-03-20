81d138ca82e728f0e1d02c3dafb88bfd13c776db
Hyeji Lee <zzambbang@gmail.com>
2022-03-20 17:16:42 +0900

    feat: user principal 추가

--- backend/src/main/java/com/record/backend/repository/PostRepository.java
+++ backend/src/main/java/com/record/backend/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.record.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.record.backend.domain.post.Post;
 
