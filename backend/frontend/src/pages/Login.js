9decd4111ddbd199c9b3f216055b798796bc85bb
Hyeji Lee <zzambbang@gmail.com>
2022-03-21 16:24:48 +0900

    feat: 로그인시 어세스 토큰 받아오는 부분 구현

--- backend/frontend/src/pages/Login.js
+++ backend/frontend/src/pages/Login.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import { login } from '../redux/actions/auth';
 
 import Box from '@mui/material/Box';
@@ -16,123 +16,131 @@ export default function Login(props) {
   const { isLoggedIn } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
+  useEffect(()=>{
+    console.log(isLoggedIn);
+  }, [isLoggedIn]);
+  
+  
   const onSubmitHandler = (e) => {
     e.preventDefault();
     console.log('email', email);
     console.log('password', password);
-
+    
     dispatch(login(email, password))
-      .then(() => {
-        navigate('/');
-        window.location.reload();
-      })
-      .catch(() => {
-        setLoading(false);
-      });
+    .then(() => {
+      console.log('isLogin', isLoggedIn);
+      navigate('/');
+      // window.location.reload();
+    })
+    .catch(() => {
+      setLoading(false);
+    });
   };
-  if (isLoggedIn) {
-    return navigate('/');
-  }
-
+  
   const onEmailHandler = (e) => {
     setEmail(e.target.value);
   };
-
+  
   const onPasswordHandler = (e) => {
     setPassword(e.target.value);
   };
-
-  return (
-    <Box
-      gap={1}
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100vw',
-        height: '100vh',
-        backgroundColor: 'white',
-      }}
-    >
-      <h2>Record:er</h2>
-
+  
+  
+  if (isLoggedIn) {
+    return <Navigate to="/" replace={true} />
+  } else{
+    return (
       <Box
-        component='form'
+        gap={1}
         sx={{
           display: 'flex',
           flexDirection: 'column',
-          '& > :not(style)': { m: 1, width: '35ch' },
-        }}
-        noValidate
-        autoComplete='off'
-        onSubmit={onSubmitHandler}
-      >
-        <TextField
-          id='outlined-basic'
-          label='이메일'
-          variant='outlined'
-          autoComplete='email'
-          autoFocus
-          onChange={onEmailHandler}
-        />
-        <TextField
-          id='outlined-password-input'
-          label='비밀번호'
-          type='password'
-          autoComplete='current-password'
-          onChange={onPasswordHandler}
-        />
-
-        {/* 로그인 정보 기억 */}
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            '& > *': {
-              m: 1,
-              width: '35ch',
-            },
-          }}
-        >
-          <FormControlLabel
-            control={<Checkbox value='로그인 정보 기억하기' color='primary' />}
-            label='Remember'
-          />
-        </Box>
-
-        {/* 로그인 버튼 */}
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            '& > *': {
-              m: 1,
-              width: '35ch',
-            },
-          }}
-        >
-          <Button variant='contained' type='submit'>
-            로그인
-          </Button>
-        </Box>
-      </Box>
-
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
+          justifyContent: 'center',
           alignItems: 'center',
-          '& > *': {
-            m: 1,
-          },
+          width: '100vw',
+          height: '100vh',
+          backgroundColor: 'white',
         }}
       >
-        <Button variant='text'>회원 가입</Button>
-        <Button variant='text'>비밀번호 찾기</Button>
+        <h2>Record:er</h2>
+  
+        <Box
+          component='form'
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            '& > :not(style)': { m: 1, width: '35ch' },
+          }}
+          noValidate
+          autoComplete='off'
+          onSubmit={onSubmitHandler}
+        >
+          <TextField
+            id='outlined-basic'
+            label='이메일'
+            variant='outlined'
+            autoComplete='email'
+            autoFocus
+            onChange={onEmailHandler}
+          />
+          <TextField
+            id='outlined-password-input'
+            label='비밀번호'
+            type='password'
+            autoComplete='current-password'
+            onChange={onPasswordHandler}
+          />
+  
+          {/* 로그인 정보 기억 */}
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              '& > *': {
+                m: 1,
+                width: '35ch',
+              },
+            }}
+          >
+            <FormControlLabel
+              control={<Checkbox value='로그인 정보 기억하기' color='primary' />}
+              label='Remember'
+            />
+          </Box>
+  
+          {/* 로그인 버튼 */}
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              '& > *': {
+                m: 1,
+                width: '35ch',
+              },
+            }}
+          >
+            <Button variant='contained' type='submit'>
+              로그인
+            </Button>
+          </Box>
+        </Box>
+  
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            '& > *': {
+              m: 1,
+            },
+          }}
+        >
+          <Button variant='text'>회원 가입</Button>
+          <Button variant='text'>비밀번호 찾기</Button>
+        </Box>
       </Box>
-    </Box>
-  );
+    );
+  }
 }
