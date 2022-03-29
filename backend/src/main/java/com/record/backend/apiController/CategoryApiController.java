8b4d0b13575a213467f922155ceec16d6ca2a706
zu21un <jwjoo03@gmail.com>
2022-03-29 14:01:38 +0900

    Fix: category 추가 및 조회 로직 수정

--- backend/src/main/java/com/record/backend/apiController/CategoryApiController.java
+++ backend/src/main/java/com/record/backend/apiController/CategoryApiController.java
@@ -1,25 +1,21 @@
 package com.record.backend.apiController;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.record.backend.domain.category.Category;
 import com.record.backend.dto.category.request.CategorySaveRequestDto;
 import com.record.backend.dto.category.request.CategoryUpdateRequestDto;
 import com.record.backend.dto.category.response.CategoryNameDto;
-import com.record.backend.dto.category.response.CategoryResponsetDto;
+import com.record.backend.dto.category.response.CategoryResponseDto;
 import com.record.backend.dto.category.response.CategoryUpdateResponseDto;
 import com.record.backend.dto.loginlogout.response.ApiResponse;
 import com.record.backend.repository.CategoryRepository;
@@ -34,13 +30,15 @@ import lombok.RequiredArgsConstructor;
 public class CategoryApiController {
 
 	private final CategoryRepository categoryRepository;
+
 	private final CategoryService categoryService;
 
 	//카테고리 생성
-	@PostMapping("/board/categories")
+	@PostMapping("/board/categories/")
 	//@ResponseBody
 	//@PreAuthorize("hasRole('USER')")
 	public ResponseEntity<?> createCategory(@RequestBody CategorySaveRequestDto requestDto) {
+
 		Category category = categoryService.saveCategory(requestDto);
 
 /*		URI location = ServletUriComponentsBuilder
@@ -50,19 +48,19 @@ public class CategoryApiController {
 		return ResponseEntity.ok(new ApiResponse(true, "Category Created Successfully"));
 	}
 
-	//카테고리 이름만 조회 -> 글 작성 폼에 뿌려주는 코드
-	@GetMapping("/board/categories")
-	public Result allCategories() {
-		List<CategoryNameDto> categoryNameDtos = categoryService.showCategoryNames();
+	// user의 모든 카테고리 목록을 반환
+	@GetMapping("/board/categories/users/{user_id}")
+	public Result findAllCategoriesByUserId(@PathVariable("user_id") Long userId) {
+		List<CategoryNameDto> categoryNameDtos = categoryService.showCategoryNames(userId);
 
 		return new Result(categoryNameDtos);
 	}
 
 	//한개만 조회
 	@GetMapping("/board/categories/{category_id}")
-	public CategoryResponsetDto findCategory(@PathVariable("category_id") Long categoryId) {
+	public CategoryResponseDto findCategory(@PathVariable("category_id") Long categoryId) {
 		Category findCategory = categoryRepository.findById(categoryId).get();
-		return new CategoryResponsetDto(findCategory);
+		return new CategoryResponseDto(findCategory);
 	}
 
 /*
