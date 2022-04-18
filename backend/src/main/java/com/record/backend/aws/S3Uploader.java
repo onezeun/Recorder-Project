92d2f1347edbb01b76781e4ef5879c0132dad6b6
Hyeji Lee <zzambbang@gmail.com>
2022-04-18 23:09:20 +0900

    fix: 유저 프로필 사진 디비 추가 안되는거 수정

--- backend/src/main/java/com/record/backend/aws/S3Uploader.java
+++ backend/src/main/java/com/record/backend/aws/S3Uploader.java
@@ -7,10 +7,15 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.record.backend.domain.user.User;
 import com.record.backend.repository.UserRepository;
@@ -21,6 +26,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 @Component
+@Service
+@Transactional(readOnly = true)
 public class S3Uploader {
 
 	private final AmazonS3Client amazonS3Client;
@@ -29,6 +36,7 @@ public class S3Uploader {
 	@Value("${cloud.aws.s3.bucket}")
 	private String bucket;
 
+	@Transactional
 	public FileUploadResponse upload(Long userId, MultipartFile multipartFile, String dirName) throws IOException {
 
 
@@ -38,17 +46,20 @@ public class S3Uploader {
 		return upload(userId, uploadFile, dirName);
 	}
 
-
+	@Transactional
 	private FileUploadResponse upload(Long userId, File uploadFile, String dirName) {
-		String fileName = dirName + "/" + uploadFile.getName();
+		String fileName = dirName + "/" + userId + "/" + "photo";
 		String uploadImageUrl = putS3(uploadFile, fileName);
 		removeNewFile(uploadFile);
+
+		System.out.println(uploadImageUrl);
 
 		User user = userRepository.findById(userId).get();
 		user.setProfilePhoto(uploadImageUrl);
 
-		return new FileUploadResponse(fileName, uploadImageUrl);
-		//return uploadImageUrl;
+		System.out.println(user.getProfilePhoto());
+
+		return new FileUploadResponse(userId, fileName, uploadImageUrl);
 	}
 
 	private String putS3(File uploadFile, String fileName) {
@@ -77,5 +88,16 @@ public class S3Uploader {
 		return Optional.empty();
 	}
 
+	// delete file
+	@Transactional
+	public void fileDelete(Long userId) {
+		/*if (!amazonS3Client.doesObjectExist(bucket, "profile/" + userId + "/photo")) {
+			throw new AmazonS3Exception("Object" + userId + "does not exist!");
+		}
+		amazonS3Client.deleteObject(new DeleteObjectRequest(bucket, "profile/" + userId + "/photo"));*/
+
+		User user = userRepository.findById(userId).get();
+		user.setProfilePhoto(null);
+	}
 
 }
