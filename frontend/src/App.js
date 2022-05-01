82f30b4873053aa7cd814d19f4d3aca156de1e05
zu21un <jwjoo03@gmail.com>
2022-05-01 19:44:12 +0900

    refactor: header 컴포넌트 refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import CssBaseline from '@mui/material/CssBaseline';
+
 import { useSelector } from "react-redux";
 import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import styled from 'styled-components';
@@ -6,7 +8,7 @@ import styled from 'styled-components';
 import './App.css';
 
 import Header from './components/Header/Header';
-import UserHeader from './components/Header/UserHeader';
+import UserHeader from './components/Header/Header';
 import MemberComponent from './components/Member/MemberConponent.js';
 import Category from './components/Category/Category';
 
@@ -26,63 +28,64 @@ const StyledDiv = styled.div`
 `;
 
 function App() {
-  const { isLoggedIn } = useSelector((state) => state.auth);
-
   return (
-    <Router>
-      {isLoggedIn ? <UserHeader /> : <Header />}
-      <StyledDiv>
-        <hr />
-          {/* <Link to="/login">
-            <button>Login</button>
-          </Link>
-          <Link to="/signup">
-            <button>Signup</button>
-          </Link> */}
-          <Link to="/editor">
-            <button>Editor</button>
-          </Link>
-          {/* <Link to="/user">
-            <button>User</button>
-          </Link> */}
-          <Link to="/post">
-            <button>Post</button>
-          </Link>
-          <Link to="/main">
-            <button>Main</button>
-          </Link>
-          {/* <Link to="/userhome">
-            <button>Userhome</button>
-          </Link> */}
-          {/* <Link to="/search">
-            <button>Search</button>
-          </Link>
-          <Link to="/neighbor">
-            <button>Neighbor</button>
-          </Link> */}
-          <Link to="/category">
-            <button>Category</button>
-          </Link>
-          {/* <Link to="/profile">
-            <button>profile</button>
-          </Link> */}
-        <hr /> 
-        <Routes>
-          <Route exact path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/editor" element={<Editor />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/post" element={<Post />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/userhome" element={<UserHome />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/neighbor" element={<Neighbor />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </StyledDiv>
-    </Router>
+    <>
+      <CssBaseline />
+      <Router>
+        <Header />
+        <StyledDiv>
+          <hr />
+            {/* <Link to="/login">
+              <button>Login</button>
+            </Link>
+            <Link to="/signup">
+              <button>Signup</button>
+            </Link> */}
+            <Link to="/editor">
+              <button>Editor</button>
+            </Link>
+            {/* <Link to="/user">
+              <button>User</button>
+            </Link> */}
+            <Link to="/post">
+              <button>Post</button>
+            </Link>
+            <Link to="/main">
+              <button>Main</button>
+            </Link>
+            {/* <Link to="/userhome">
+              <button>Userhome</button>
+            </Link> */}
+            {/* <Link to="/search">
+              <button>Search</button>
+            </Link>
+            <Link to="/neighbor">
+              <button>Neighbor</button>
+            </Link> */}
+            <Link to="/category">
+              <button>Category</button>
+            </Link>
+            {/* <Link to="/profile">
+              <button>profile</button>
+            </Link> */}
+          <hr /> 
+          <Routes>
+            <Route exact path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/editor" element={<Editor />} />
+            <Route path="/user" element={<User />} />
+            <Route path="/post" element={<Post />} />
+            <Route path="/main" element={<Main />} />
+            <Route path="/userhome" element={<UserHome />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/neighbor" element={<Neighbor />} />
+            <Route path="/category" element={<Category />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </StyledDiv>
+      </Router>
+    </>
   );
 }
 
