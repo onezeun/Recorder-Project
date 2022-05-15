3826029bd4cd6f4a27dd1015d11a4df7c9159e62
Hyeji Lee <zzambbang@gmail.com>
2022-05-15 19:17:56 +0900

    fix: 카테고리 생성 수정 중복성 검사 오류 수정

--- backend/src/main/java/com/record/backend/repository/CategoryRepository.java
+++ backend/src/main/java/com/record/backend/repository/CategoryRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.record.backend.domain.category.Category;
 
@@ -12,5 +14,11 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 	Optional<Category> findByName(String categoryName);
 
 	List<Category> findAllByUserId(Long userId);
+
+	@Query("SELECT c.name FROM Category c WHERE c.user.id = :userId and c.name = :categoryName")
+	String existCategoryByUserId(@Param("userId") Long userId, @Param("categoryName") String categoryName);
+
+	@Query("SELECT c.user.id FROM Category c WHERE c.id = :categoryId")
+	Long findUserIdByCategoryId(@Param("categoryId") Long categoryId);
 }
 
